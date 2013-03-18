c2cc4f7f4ed6d134f3f56f9f306bd3b99e137f00
Bulat <bulat.zamilov@gmail.com>
2013-03-18 09:47:28 +0800

    added documentation to functions and script

--- track.py
+++ track.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 # track.py
 
-__author__    = 'cyberbolt <cyberbolt@rambler.ru>'
-__version__   = '$Revision: 0.0.1 $'
-__date__      = '$Date: 2013 March 17$'
-__copyright__ = 'Bulat Zamilov'
+"""This script does logging of your activity. Each time you run it with yor activity as argument
+it adds time in seconds passed since UNIX Epoch started"""
+
+__author__    = 'Bulat Zamilov <bulat.zamilov@gmail.com>'
+__version__   = '0.0.1'
+__date__      = '2013 March 18'
+__copyright__ = 'Copyright (c) 2013 Bulat Zamilov' 
 __license__   = 'GPLv2'
 
 from sys import argv
@@ -13,12 +16,13 @@ from time import time
 from math import floor
 
 def help():
+    """Prints how to use track.py script"""
     print """
     Usage: ./timetrack TASK_NAME
     """ 
 
-if __name__ == '__main__':
-    
+def main():
+    """Reads arguments and puts time stamps into timelog.log"""
     if len(argv) == 1 or len(argv) > 2:
         help()
     else:
@@ -28,3 +32,6 @@ if __name__ == '__main__':
         log.write(str(int(floor(time()))) + '\n')
         log.write(argv[1] + delimiter + str(int(floor(time()))) + delimiter)
         log.close()
+
+if __name__ == '__main__':
+    main()
